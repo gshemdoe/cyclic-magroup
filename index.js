310f6a46238f50e93a84ead9d44e1b4b0c8b2ef4
gshemdoe <georgehmariki@gmail.com>
2023-08-10 03:40:56 +0300

    updated

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ const getRoutes = require('./routes/get')
 const bots = require('./bots/bot')
 
 // database connection
-mongoose.connect(`mongodb://${process.env.USER}:${process.env.PASS}@nodetuts-shard-00-00.ngo9k.mongodb.net:27017,nodetuts-shard-00-01.ngo9k.mongodb.net:27017,nodetuts-shard-00-02.ngo9k.mongodb.net:27017/bongo-p1?authSource=admin&replicaSet=atlas-pyxyme-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true`)
-    .then(() => console.log('Connected to Bongo P1 DB'))
-    .catch((err) => {
-        console.log(err)
-    })
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(`mongodb://${process.env.USER}:${process.env.PASS}@nodetuts-shard-00-00.ngo9k.mongodb.net:27017,nodetuts-shard-00-01.ngo9k.mongodb.net:27017,nodetuts-shard-00-02.ngo9k.mongodb.net:27017/bongo-p1?authSource=admin&replicaSet=atlas-pyxyme-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true`)
+        console.log('Connected to BONGO P1 BOTS DB on ' + conn.connection.host)
+    } catch (error) {
+        console.log(error.message, error)
+    }
+}
 
 // middlewares
 app.set('view engine', 'ejs')
@@ -28,4 +31,6 @@ if (process.env.ENVIRONMENT == 'production') {
 }
 
 
-app.listen(process.env.PORT || 3000, () => console.log('Listen to port 3000'))
+connectDB().then(() => {
+    app.listen(process.env.PORT || 3000, () => console.log('Listen to port 3000'))
+})
